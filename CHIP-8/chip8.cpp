dcef57d21771765051a1064993367e0a6fecf125
Marco Aguilera <marcoaguilera@Marcos-Air-2.hsd1.ca.comcast.net>
2018-01-16 21:41:48 -0800

    Mostly finished the header. Tested out the buffer.

--- CHIP-8/chip8.cpp
+++ CHIP-8/chip8.cpp
@@ -7,3 +7,52 @@
 //
 
 #include "chip8.hpp"
+
+void Chip8::initialize()
+{
+    pC = 0x200;
+    opcode = 0;
+    iR = 0;
+    stack_pointer = 0;
+    
+    // SDL_CLEAR *** NEEDS TO STILL BE DONE ***
+    
+    // CLEAR STACK
+    for(unsigned i = 0; i < 16; i++)
+    {
+        stack[i] = 0;
+    }
+    
+    // CLEAR V0 - VF REGISTERS
+    for(unsigned i = 0; i < 16; i++)
+    {
+        vRegisters[i] = 0;
+    }
+    
+    // CLEAR ALL MEMORY
+    for(unsigned i = 0; i < 4096; i++)
+    {
+        memory[i] = 0;
+    }
+    
+    // LOAD FONTSET
+    for(unsigned i = 0; i < 80; i++)
+    {
+        memory[i] = chip8_fontset[i];
+    }
+    
+    // RESET TIMERS *** NEEDS TO STILL BE DONE WITH SDL ***
+    
+    // LOAD GAME
+    for(unsigned i = 0; i < bufferSize; i++)
+    {
+        memory[i + 512] = buffer[i];
+    }
+    
+    
+    
+}
+void Chip8::emulateCycle()
+{
+    
+}
--- CHIP-8/main.cpp
+++ CHIP-8/main.cpp
@@ -7,10 +7,57 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sys/stat.h>
 using namespace std;
-int main() {
-    unsigned char name = 0xF1;
+
+//RETURNS NUMBER OF BYTES IN ROM FILE
+size_t getRomSize(const char* filename)
+{
+    struct stat st;
     
-    cout << name << endl;
+    if(stat(filename, &st) != 0)
+    {
+        return -1;
+    }
+    return st.st_size;
+}
+
+typedef unsigned char BYTE;
+int main()
+{
+
+//    ifstream input("/Users/marcoaguilera/Desktop/c8games/BLITZ", ios::binary);
+//    vector<unsigned char> buffer((istream_iterator<unsigned char>(input)), (istream_iterator<unsigned char>()));
+//    
+//    cout << buffer.size() << endl;
+//    
+//    for(auto hex : buffer)
+//    {
+//        cout << int(hex) << endl;
+//    }
+//    input.close();
+//    
+
+//    size_t size = getRomSize("/Users/marcoaguilera/Desktop/c8games/BLITZ");
+//    char buffer[4096];
+//    ifstream file("/Users/marcoaguilera/Desktop/c8games/UFO", ios::in | ios::binary);
+//    file.read(buffer, size);
+//    
+//    if(!file)
+//    {
+//        cout << "NUMBER OF BYTES READ : " << file.gcount() << endl;
+//        file.clear();
+//    }
+//    
+//    for(unsigned i = 0; i < size; i++)
+//    {
+//        cout << int(buffer[i]) << endl;
+//    }
+//    
+//    cout << size << endl;
+//    file.close();
+    
     return 0;
 }

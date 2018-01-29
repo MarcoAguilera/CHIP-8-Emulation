3107a494d8fa89557643909475464eab5b0b9748
Marco Aguilera <marcoaguilera@Marcos-MacBook-Air-2.local>
2018-01-29 15:14:57 -0800

    Added Opcodes 8XY0 - 8XY7 and 8XYE. Also added 9XY0, ANNN, BNNN, and CXNN

--- CHIP-8/chip8.cpp
+++ CHIP-8/chip8.cpp
@@ -110,30 +110,88 @@ void Chip8::emulateCycle()
             vRegisters[(opcode >> 8) & 0x0F] += opcode & 0x00FF;
             break;
         case 0x8000:
-            switch(opcode &  0x000F){
+            switch(opcode & 0x000F){
                 case 0x0:
-                    
+                    vRegisters[(opcode >> 8) & 0x0F] = opcode >> 4 & 0x00F;
+                    pC += 2;
                     break;
                 case 0x1:
+                    vRegisters[(opcode >> 8) & (0x0F)] = (((opcode >> 8) & (0x0F)) | ((opcode >> 4) & 0x00F));
+                    pC += 2;
                     break;
                 case 0x2:
+                    vRegisters[(opcode >> 8) & (0x0F)] = (((opcode >> 8) & (0x0F)) & ((opcode >> 4) & 0x00F));
+                    pC += 2;
                     break;
                 case 0x3:
+                    vRegisters[(opcode >> 8) & (0x0F)] = (((opcode >> 8) & (0x0F)) ^ ((opcode >> 4) & 0x00F));
+                    pC += 2;
                     break;
                 case 0x4:
+                    if((vRegisters[opcode >> 8 & 0x0F]) + (vRegisters[opcode >> 4 & 0x0F]) > 255){
+                        vRegisters[15] = 1;
+                    }
+                    else{
+                        vRegisters[15] = 0;
+                    }
+                    vRegisters[opcode >> 8 & 0x0F] += vRegisters[opcode >> 4 & 0x0F];
+                    pC += 2;
                     break;
                 case 0x5:
+                    if((opcode >> 8 & 0x0F) > (opcode >> 4 & 0x00F)){
+                        vRegisters[15] = 1;
+                    }
+                    else{
+                        vRegisters[15] = 0;
+                    }
+                    vRegisters[opcode >> 8 & 0x0F] -= vRegisters[opcode >> 4 & 0x00F];
+                    pC += 2;
                     break;
                 case 0x6:
+                    vRegisters[15] = vRegisters[opcode >> 4 & 0x00F] & 0x0001;
+                    vRegisters[opcode >> 8 & 0x0F] = vRegisters[opcode >> 4 & 0x00F];
+                    pC += 2;
                     break;
                 case 0x7:
+                    if(vRegisters[opcode >> 8 & 0x0F] > vRegisters[opcode >> 4 & 0x00F]){
+                        vRegisters[15] = 0;
+                    }
+                    else{
+                        vRegisters[15] = 1;
+                    }
+                    
+                    vRegisters[opcode >> 8 & 0x0F] -= vRegisters[opcode >> 4 & 0x00F];
+                    pC += 2;
                     break;
                 case 0xE:
+                    vRegisters[15] = vRegisters[opcode >> 4 & 0x00F] & 0x8000;
+                    vRegisters[opcode >> 8 & 0x0F] = vRegisters[opcode >> 4 & 0x00F] << 1;
+                    pC += 2;
                     break;
                 default:
                     cout << "ERROR!\n";
                     break;
             }
+        case 0x9000:
+            if(vRegisters[opcode >> 8 & 0x0F] != vRegisters[opcode >> 4 & 0x00F]){
+                pC += 4;
+            }
+            else{
+                pC += 2;
+            }
+            break;
+        case 0xA000:
+            iR = opcode & 0x0FFF;
+            pC += 2;
+            break;
+        case 0xB000:
+            pC = vRegisters[0] + (opcode & 0x0FFF);
+            break;
+        case 0xC000:
+            vRegisters[opcode >> 8 & 0x0F] = rand() % 256 & (opcode & 0x00FF);
+            pC += 2;
+            break;
+        
     }
 }
 
